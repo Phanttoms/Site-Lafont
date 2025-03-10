93052f6e6b12400825aeb98759f5bc335d1aebff
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-10 11:43:41 +0100

    Ajout dépendances

---
+++ front-end/src/Pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<>
+			<div>coucou V2</div>
+		</>
+	);
+}
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// style
+import "./_main.scss";
+
+// chemin des pages
+import RoutesPath from "./Router";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RoutesPath />
+	</StrictMode>
+);
