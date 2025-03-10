2c007c59e49f38cfc93a316baec4c462086987dd
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-10 13:39:34 +0100

    Ajout presentation

--- front-end/src/Pages/Home/index.tsx
+++ front-end/src/Pages/Home/index.tsx
@@ -1,10 +1,12 @@
 // composants de la page
 import Header from "../../components/Header";
+import Presentation from "../../components/Presentation";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
+			<Presentation />
 		</>
 	);
 }
---
+++ front-end/src/components/Presentation/index.tsx
@@ -0,0 +1,18 @@
+// style
+import "./_presentation.scss";
+
+export default function Presentation() {
+	return (
+		<article className="presentation">
+			<h2 className="presentation--title">
+				Ferrailleur local dans la Somme depuis 30 ans
+			</h2>
+			<p className="presentation--text">
+				Nous vous débarrassons de toutes vos férailles et électroménager en
+				petite et grande quantité, enlèvons vos épaves de voiture et vous
+				débarrassons de la cave au grenier. Découpe chalumeau, bâtiment
+				agricole, vide maison integralement.
+			</p>
+		</article>
+	);
+}
