4e8325fa58dbee4270edb74eea829fca2386bfcb
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-11 12:17:24 +0100

    Ajout exemple

--- front-end/src/Pages/Home/index.tsx
+++ front-end/src/Pages/Home/index.tsx
@@ -1,12 +1,14 @@
 // composants de la page
 import Header from "../../components/Header";
 import Presentation from "../../components/Presentation";
+import Exemple from "../../components/Exemple";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<Presentation />
+			<Exemple />
 		</>
 	);
 }
---
+++ front-end/src/components/Exemple/index.tsx
@@ -0,0 +1,39 @@
+// style
+import "./_exemple.scss";
+
+// images
+import exemple from "../../assets/images/exemple/Exemple01.png";
+
+export default function Exemple() {
+	return (
+		<article className="exemple">
+			<div className="exemple__content">
+				<img
+					className="exemple__content--img"
+					src={exemple}
+					alt="Exemple de récupération"
+				/>
+				<div className="exemple__content--text">
+					<p>7j/7j</p>
+					<p>Travail propre et rapide</p>
+				</div>
+			</div>
+			<div className="exemple__description">
+				<h2>Récupération féraille et tous métaux</h2>
+				<ul>
+					<li>Batterie</li>
+					<li>Fonte</li>
+					<li>Alu</li>
+					<li>Inox</li>
+					<li>Cuivre</li>
+					<li>Zinc</li>
+					<li>Plomb</li>
+					<li>Enlèvement d'épaves</li>
+					<li>Débarras de caves</li>
+					<li>Greniers</li>
+					<li>Garages</li>
+				</ul>
+			</div>
+		</article>
+	);
+}
