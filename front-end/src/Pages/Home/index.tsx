ca8e42f6546f8d301292b9164be8b00237564d8a
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-11 13:15:32 +0100

    Ajout footer

--- front-end/src/Pages/Home/index.tsx
+++ front-end/src/Pages/Home/index.tsx
@@ -3,6 +3,7 @@ import Header from "../../components/Header";
 import Presentation from "../../components/Presentation";
 import Exemple from "../../components/Exemple";
 import Contact from "../../components/Contact";
+import Footer from "../../components/Footer";
 
 export default function Home() {
 	return (
@@ -11,6 +12,7 @@ export default function Home() {
 			<Presentation />
 			<Exemple />
 			<Contact />
+			<Footer />
 		</>
 	);
 }
---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+// style
+import "./_footer.scss";
+
+export default function Footer() {
+	return (
+		<footer className="footer">
+			<p className="footer--text">
+				Site créé par{" "}
+				<a
+					href="https://portfolio-yohan-pruche.vercel.app/"
+					target="_blank"
+					rel="noopener"
+				>
+					Pruche Yohan
+				</a>
+			</p>
+		</footer>
+	);
+}
