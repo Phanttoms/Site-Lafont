aa58bd81614421084fef9dcb3bddc9a31f2a44ad
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-14 14:11:50 +0100

    Ajout img 800px

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -9,6 +9,8 @@ export default function Header() {
 		<header className="header">
 			<img
 				className="header--img"
+				srcSet="/images/banner/Banner800px.webp 600w, /images/banner/Banner01.webp 1200w"
+				sizes="(max-width: 600px) 100vw, 1200px"
 				src="/images/banner/Banner01.webp"
 				alt="Image de bannière"
 			/>
