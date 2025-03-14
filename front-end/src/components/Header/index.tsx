126bb8b001edf95c00d89be269f1570fa83805cd
Yohan Pruche <Yohan.Pruche@orange.fr>
2025-03-14 13:47:15 +0100

    Test banner public

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -2,12 +2,16 @@
 import "./_header.scss";
 
 // images
-import banner from "../../assets/images/banner/Banner01.webp";
+// import banner from "../../assets/images/banner/Banner01.webp";
 
 export default function Header() {
 	return (
 		<header className="header">
-			<img className="header--img" src={banner} alt="Image de bannière" />
+			<img
+				className="header--img"
+				src="/images/banner/Banner01.webp"
+				alt="Image de bannière"
+			/>
 			<div className="header__content">
 				<h1 className="header__content--title">David Lafont</h1>
 				<p className="header__content--text">
